3a2f49bbdb148191282226a2e6d42f73cccf07e1
Andy Willis <awillis@fastmail.fm>
2023-05-04 17:20:46 +0100

    Updated

--- example/index.js
+++
@@ -1,8 +0,0 @@
-import { readFile } from 'fs/promises';
-import journalMarkdownConverter from '../src/index';
-
-const markdown = await readFile('./data/journal.md', 'utf8');
-
-const json = await journalMarkdownConverter(markdown);
-
-console.dir(json, { depth: null });
